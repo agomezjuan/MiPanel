b6d8e46c9e637b9a3b38648ee31e09fb0e4f360c
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-11-08 09:29:59 +0100

    feat: avatar barrel

---
+++ src/assets/avatars/index.js
@@ -0,0 +1,6 @@
+import { default as user1 } from "./user1.png";
+import { default as user2 } from "./user2.png";
+import { default as user3 } from "./user3.png";
+import { default as user4 } from "./user4.png";
+
+export const avatars = [user1, user2, user3, user4];

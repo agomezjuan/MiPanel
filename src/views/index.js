ac729e80b05bcc82f279c41ecd9d75b386ff4e20
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-11-08 09:35:59 +0100

    chore: views barrel

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+export { default as Contact } from "./Contact";
+export { default as EditUser } from "./EditUser";
+export { default as Home } from "./Home";
+export { default as NewUser } from "./NewUser";
+export { default as NotFound } from "./NotFound";
+export { default as Table } from "./Table";
+export { default as ViewUser } from "./ViewUser";
